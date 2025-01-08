6b5d5ae6fd3f06e37b6638a3edf23fbcfad9ca2b
GabrieleFerrero <gabriele.ferrero@polito.it>
2025-01-08 10:59:16 +0100

    add a warning in analytical_solid_inv refactor inventory to match cases

--- tools/component_tools.py
+++ tools/component_tools.py
@@ -1854,155 +1854,10 @@ class Component:
 
     def analytical_solid_inventory(self, p_out: float = 0):
         if self.fluid.k_t is None:
+            print("computing mass transfer coefficient")
             self.fluid.get_kt(turbulator=self.geometry.turbulator)
-        if self.fluid.MS == False:
-
-            def integralfun(r):
-                return (
-                    1
-                    / 4
-                    * r**2
-                    * (2 * np.log(r / (self.geometry.D / 2 + self.geometry.thick)) - 1)
-                )
-
-            def circle(r):
-                return np.pi * r**2
-
-            dimless = (
-                2
-                * self.membrane.D
-                * self.membrane.K_S
-                / (
-                    self.fluid.k_t
-                    * self.fluid.Solubility
-                    * self.fluid.d_Hyd
-                    * np.log(
-                        (self.fluid.d_Hyd + 2 * self.membrane.thick) / self.fluid.d_Hyd
-                    )
-                )
-            )
-            dimless2 = (
-                2
-                * self.membrane.D
-                * self.membrane.K_S
-                / (
-                    self.fluid.Solubility
-                    * self.fluid.d_Hyd
-                    * np.log(
-                        (self.fluid.d_Hyd + 2 * self.membrane.thick) / self.fluid.d_Hyd
-                    )
-                )
-            )
-            L_ch = (
-                -dimless
-                / (1 + dimless)
-                * 4
-                * self.fluid.k_t
-                / (self.fluid.U0 * self.fluid.d_Hyd)
-            )
-            K = (
-                -2
-                * np.pi
-                * (
-                    self.c_in
-                    / (dimless2 / self.fluid.k_t + 1)
-                    / self.fluid.Solubility
-                    * self.membrane.K_S
-                )
-                / np.log(
-                    (self.geometry.D / 2 + self.geometry.thick) / (self.geometry.D / 2)
-                )
-            )
-            L_factor = (np.exp(L_ch * self.geometry.L) - 1) / L_ch
-            K = K * L_factor
-            integral = (
-                K * integralfun(self.geometry.D / 2 + self.geometry.thick)
-                - K * integralfun(self.geometry.D / 2)
-            ) + self.geometry.L * p_out**0.5 * self.membrane.K_S * (
-                circle(self.geometry.D / 2 + self.geometry.thick)
-                - circle(self.geometry.D / 2)
-            )
-            inventory = integral
-            self.membrane.inv = inventory
-            return inventory
-        else:
-            print("not implemented yet")
-
-            def ms_integral(self, p_out: float = 0, L: float = 0):
-                tau = 4 * self.fluid.k_t * L / (self.fluid.U0 * self.fluid.d_Hyd)
-                self.xi = (
-                    1
-                    / self.c_in
-                    / self.fluid.Solubility
-                    * (
-                        0.5  ##TODO: Check this
-                        * self.membrane.K_S
-                        * self.membrane.D
-                        / (
-                            self.fluid.k_t
-                            * self.fluid.d_Hyd
-                            * np.log(
-                                (self.fluid.d_Hyd + 2 * self.membrane.thick)
-                                / self.fluid.d_Hyd
-                            )
-                        )
-                    )
-                    ** 2
-                )
-
-                beta = (1 / self.xi + 1) ** 0.5 + np.log((1 / self.xi + 1) ** 0.5 - 1)
-                max_exp = np.log(np.finfo(np.float64).max)
-                beta_tau = beta - tau - 1
-                if beta_tau > max_exp:
-                    # print(
-                    #     "Warning: Overflow encountered in exp, input too large.Approximation triggered. This will slow down the calculation"
-                    # )
-
-                    w = beta_tau - np.log(beta_tau)
-                    w2 = -beta_tau
-
-                else:
-                    z = np.exp(beta_tau)
-                    z2 = np.exp(-beta_tau)
-                    w = lambertw(z, tol=1e-10)
-                    w2 = lambertw(z2, tol=1e-10)
-                    if w.imag != 0:
-                        raise ValueError("self.eff_an has a non-zero imaginary part")
-                    if w2.imag != 0:
-                        raise ValueError("self.eff_an has a non-zero imaginary part")
-                    w = w.real
-                    w2 = w2.real
-                alpha = (
-                    1
-                    / self.fluid.Solubility
-                    * (
-                        (0.5 * self.membrane.D * self.membrane.K_S)  ## TODO: Check this
-                        / (
-                            self.fluid.k_t
-                            * self.fluid.d_Hyd
-                            * np.log(
-                                (self.fluid.d_Hyd + 2 * self.membrane.thick)
-                                / self.fluid.d_Hyd
-                            )
-                        )
-                    )
-                    ** 2
-                )
-                c_ext = p_out**0.5 * self.membrane.K_S
-                conv = (self.c_in / self.fluid.Solubility) ** 0.5 * self.membrane.K_S
-                c_w_l = alpha * (w**2 + 2 * w) + alpha * (
-                    2 - 2 * ((w**2 + 2 * w) + 1) ** 0.5
-                )  ## TODO: Check this
-                K = (
-                    alpha**0.5
-                    / self.fluid.Solubility**0.5
-                    * (
-                        -beta_tau
-                        * (w**2 - w + 1)
-                        / (4 * self.fluid.k_t / (self.fluid.U0 * self.fluid.d_Hyd) * w2)
-                    )
-                    * self.membrane.K_S
-                )
+        match self.fluid.MS:
+            case False:
 
                 def integralfun(r):
                     return (
@@ -2015,34 +1870,205 @@ class Component:
                         )
                     )
 
-                integral = K * integralfun(
-                    self.geometry.D / 2 + self.geometry.thick
-                ) - K * integralfun(self.geometry.D / 2)
-                return integral
-
-            def p_out_term(self, p_out):
                 def circle(r):
                     return np.pi * r**2
 
-                add = (
-                    self.geometry.L
-                    * p_out**0.5
+                dimless = (
+                    2
+                    * self.membrane.D
                     * self.membrane.K_S
-                    * (
-                        circle(self.geometry.D / 2 + self.geometry.thick)
-                        - circle(self.geometry.D / 2)
+                    / (
+                        self.fluid.k_t
+                        * self.fluid.Solubility
+                        * self.fluid.d_Hyd
+                        * np.log(
+                            (self.fluid.d_Hyd + 2 * self.membrane.thick)
+                            / self.fluid.d_Hyd
+                        )
                     )
                 )
+                dimless2 = (
+                    2
+                    * self.membrane.D
+                    * self.membrane.K_S
+                    / (
+                        self.fluid.Solubility
+                        * self.fluid.d_Hyd
+                        * np.log(
+                            (self.fluid.d_Hyd + 2 * self.membrane.thick)
+                            / self.fluid.d_Hyd
+                        )
+                    )
+                )
+                L_ch = (
+                    -dimless
+                    / (1 + dimless)
+                    * 4
+                    * self.fluid.k_t
+                    / (self.fluid.U0 * self.fluid.d_Hyd)
+                )
+                K = (
+                    -2
+                    * np.pi
+                    * (
+                        self.c_in
+                        / (dimless2 / self.fluid.k_t + 1)
+                        / self.fluid.Solubility
+                        * self.membrane.K_S
+                    )
+                    / np.log(
+                        (self.geometry.D / 2 + self.geometry.thick)
+                        / (self.geometry.D / 2)
+                    )
+                )
+                L_factor = (np.exp(L_ch * self.geometry.L) - 1) / L_ch
+                K = K * L_factor
+                integral = (
+                    K * integralfun(self.geometry.D / 2 + self.geometry.thick)
+                    - K * integralfun(self.geometry.D / 2)
+                ) + self.geometry.L * p_out**0.5 * self.membrane.K_S * (
+                    circle(self.geometry.D / 2 + self.geometry.thick)
+                    - circle(self.geometry.D / 2)
+                )
+                inventory = integral
+                self.membrane.inv = inventory
+                return inventory
+            case True:
+                print("not implemented yet")
 
-                return add
+                def ms_integral(self, p_out: float = 0, L: float = 0):
+                    tau = 4 * self.fluid.k_t * L / (self.fluid.U0 * self.fluid.d_Hyd)
+                    self.xi = (
+                        1
+                        / self.c_in
+                        / self.fluid.Solubility
+                        * (
+                            0.5  ##TODO: Check this
+                            * self.membrane.K_S
+                            * self.membrane.D
+                            / (
+                                self.fluid.k_t
+                                * self.fluid.d_Hyd
+                                * np.log(
+                                    (self.fluid.d_Hyd + 2 * self.membrane.thick)
+                                    / self.fluid.d_Hyd
+                                )
+                            )
+                        )
+                        ** 2
+                    )
 
-            inv = (
-                ms_integral(self=self, L=self.geometry.L, p_out=p_out)
-                - ms_integral(self=self, L=0, p_out=p_out)
-                + p_out_term(self, p_out)
-            )
-            self.membrane.inv = inv
-            return inv
+                    beta = (1 / self.xi + 1) ** 0.5 + np.log(
+                        (1 / self.xi + 1) ** 0.5 - 1
+                    )
+                    max_exp = np.log(np.finfo(np.float64).max)
+                    beta_tau = beta - tau - 1
+                    if beta_tau > max_exp:
+                        # print(
+                        #     "Warning: Overflow encountered in exp, input too large.Approximation triggered. This will slow down the calculation"
+                        # )
+
+                        w = beta_tau - np.log(beta_tau)
+                        w2 = -beta_tau
+
+                    else:
+                        z = np.exp(beta_tau)
+                        z2 = np.exp(-beta_tau)
+                        w = lambertw(z, tol=1e-10)
+                        w2 = lambertw(z2, tol=1e-10)
+                        if w.imag != 0:
+                            raise ValueError(
+                                "self.eff_an has a non-zero imaginary part"
+                            )
+                        if w2.imag != 0:
+                            raise ValueError(
+                                "self.eff_an has a non-zero imaginary part"
+                            )
+                        w = w.real
+                        w2 = w2.real
+                    alpha = (
+                        1
+                        / self.fluid.Solubility
+                        * (
+                            (
+                                0.5 * self.membrane.D * self.membrane.K_S
+                            )  ## TODO: Check this
+                            / (
+                                self.fluid.k_t
+                                * self.fluid.d_Hyd
+                                * np.log(
+                                    (self.fluid.d_Hyd + 2 * self.membrane.thick)
+                                    / self.fluid.d_Hyd
+                                )
+                            )
+                        )
+                        ** 2
+                    )
+                    c_ext = p_out**0.5 * self.membrane.K_S
+                    conv = (
+                        self.c_in / self.fluid.Solubility
+                    ) ** 0.5 * self.membrane.K_S
+                    c_w_l = alpha * (w**2 + 2 * w) + alpha * (
+                        2 - 2 * ((w**2 + 2 * w) + 1) ** 0.5
+                    )  ## TODO: Check this
+                    K = (
+                        alpha**0.5
+                        / self.fluid.Solubility**0.5
+                        * (
+                            -beta_tau
+                            * (w**2 - w + 1)
+                            / (
+                                4
+                                * self.fluid.k_t
+                                / (self.fluid.U0 * self.fluid.d_Hyd)
+                                * w2
+                            )
+                        )
+                        * self.membrane.K_S
+                    )
+
+                    def integralfun(r):
+                        return (
+                            1
+                            / 4
+                            * r**2
+                            * (
+                                2
+                                * np.log(
+                                    r / (self.geometry.D / 2 + self.geometry.thick)
+                                )
+                                - 1
+                            )
+                        )
+
+                    integral = K * integralfun(
+                        self.geometry.D / 2 + self.geometry.thick
+                    ) - K * integralfun(self.geometry.D / 2)
+                    return integral
+
+                def p_out_term(self, p_out):
+                    def circle(r):
+                        return np.pi * r**2
+
+                    add = (
+                        self.geometry.L
+                        * p_out**0.5
+                        * self.membrane.K_S
+                        * (
+                            circle(self.geometry.D / 2 + self.geometry.thick)
+                            - circle(self.geometry.D / 2)
+                        )
+                    )
+
+                    return add
+
+                inv = (
+                    ms_integral(self=self, L=self.geometry.L, p_out=p_out)
+                    - ms_integral(self=self, L=0, p_out=p_out)
+                    + p_out_term(self, p_out)
+                )
+                self.membrane.inv = inv
+                return inv
 
     def get_solid_inventory(self, p_out: float = 0, flag_an=False):
         if flag_an:
